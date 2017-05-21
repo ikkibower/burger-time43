2981a0aeeb21bef3654e06f7d1db292f8f6de252
Elliott Chalmers <elliottchalmers@elliotts-mbp.attlocal.net>
2017-05-21 18:31:41 -0400

    sql1

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 var path = require('path');
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com    ",
-    user: "pyigd0vxl4080c21",
-    password: "cicz4pddpz5ryh2m",
-    database: "burgers_db"
-});
+var connection = mysql.mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) {

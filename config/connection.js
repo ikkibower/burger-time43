09ba1f181951e413e18ede83b90823f2828bdd0c
Elliott Chalmers <elliottchalmers@elliotts-mbp.attlocal.net>
2017-05-21 20:42:17 -0400

    sql2

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,19 @@
 var path = require('path');
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+		var connection = mysql.createConnection({
+	    host: 'localhost',
+	    port: 3306,
+	    user: 'root',
+	    password: "root",
+	    database: "burgers_db"
+	});
 
+}
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);

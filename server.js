c8ac957aff25f0d73d806024c1ca1ee2fa23f940
Elliott Chalmers <elliottchalmers@elliotts-mbp.attlocal.net>
2017-05-21 21:13:41 -0400

    sql3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public'));
 
 app.use(methodOverride("_method"));
 
